5a903b861bdd0448edbb2ddddf0bb958df5d1afd
kunalmkv <kunalm4.kvtagore@gmail.com>
2025-04-04 02:07:24 +0530

    fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
-// rollup.config.js
-const resolve = require("@rollup/plugin-node-resolve")
-const commonjs = require("@rollup/plugin-commonjs")
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json'); // Required for JSON files
 
 module.exports = {
-  input: "src/sdk.js", // Replace with the correct entry point for your SDK
+  input: 'src/sdk.js',  // Path to your main entry file for the SDK
   output: {
-    file: "dist/ethers-batch-sdk.js",
-    format: "umd", // Use 'umd' for universal module format
-    name: "BatchSDK",
+    file: 'dist/flowBatcher.js',
+    format: 'umd',       // Universal Module Definition format
+    name: 'BatchSDK',     // The global name for UMD
   },
-  plugins: [resolve(), commonjs()],
-}
+  plugins: [
+    resolve(),            // Resolves external dependencies in node_modules
+    commonjs(),           // Converts CommonJS modules to ES6 modules
+    json(),               // Enables importing of JSON files
+  ],
+};
